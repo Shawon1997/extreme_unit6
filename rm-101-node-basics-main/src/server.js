4087ef2f7088166f297b35fc1358b3f257755e1d
Shawon1997 <16haw87@gmail.com>
2022-06-20 18:45:44 +0530

    filee add

---
+++ rm-101-node-basics-main/src/server.js
@@ -0,0 +1,9 @@
+const express =require("express")
+const express = () => {};
+let app = express();
+
+
+app.listen(8000,()=>{
+    console.log("i am connected in port 8000")
+})
+module.exports = app;
